5bd4d8a170787c72eb2a014005dcd571c1146932
James Hinton <james.hinton@live.co.uk>
2024-03-02 16:26:51 +0000

    feat: add public access block to S3 bucket

--- s3.tf
+++ s3.tf
@@ -6,7 +6,11 @@ resource "aws_s3_bucket" "aurora_data" {
   }
 }
 
-resource "aws_s3_bucket_acl" "aurora_data_acl" {
-  bucket = aws_s3_bucket.aurora_data.id
-  acl    = "private"
+resource "aws_s3_bucket_public_access_block" "aurora_data_public_access_block" {
+  bucket                  = aws_s3_bucket.aurora_data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
