ec8da5aa0b06bd432180f8a3b37eba08318f2209
James Hinton <james.hinton@live.co.uk>
2024-03-03 15:05:38 +0000

    refactor: lambda function for retrieving data

--- lambda.tf
+++ lambda.tf
@@ -26,29 +26,19 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-# Lambda function for processing data
-resource "aws_lambda_function" "process_data_lambda" {
-  function_name = "process_data"
+# Lambda function for retrieving data
+resource "aws_lambda_function" "retrieve_data" {
+  function_name = "retrieve_data"
 
   s3_bucket        = "aurora-explorer-data"
   s3_key           = "package.zip"
-
 
   handler = "handler.lambda_handler"
   runtime = "python3.10"
 
   role = aws_iam_role.lambda_execution_role.arn
+  timeout = 60
   
   # Important note that this assumes the package.zip file is located there.
   source_code_hash = filebase64sha256("${path.module}/../aurora-explorer-lambda-functions/package.zip")
-
-  # Credit to lambgeo/lambda-gdal for the layer
-  layers = ["arn:aws:lambda:eu-west-2:524387336408:layer:gdal38:1"]
-
-  environment {
-    variables = {
-      GDAL_DATA = "/opt/share/gdal"
-      PROJ_LIB  = "/opt/share/proj"
-    }
-  }
 }
